02612dcaa38c97903906d5a0ebd8afd3bbb41d87
Albin Cederblad <albin.cederblad@hotmail.com>
2020-06-14 22:05:32 +0200

    delete board button

--- src/components/BoardCollection/BoardTile.js
+++ src/components/BoardCollection/BoardTile.js
@@ -1,16 +1,42 @@
 import React from 'react';
+
+// UI
 import styled from 'styled-components';
 import FadeIn from 'react-fade-in';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+// actions
+import { deleteBoard } from '../../actions';
+import { useDispatch } from "react-redux";
+
+
+
+const TileOptions = styled.div`
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    visibility: hidden;
+    display: inline-block;
+    margin: 5px;
+    border-radius: .3rem;
+    padding: 0.4rem 0.6rem 0.4rem 0.6rem;
+    color: #172b4d;
+    opacity: 0.4;
+    
+    &:hover {
+        background-color: #e1e2e6;
+    }
+`;
 const Tile = styled.div`
+    position: relative;
     display: flex;
     height: 80px;
     width: 160px;
     border-radius: 5px;
     flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+    align-items: left;
+    text-align: left;
     color: #fff;
     background-color: #519839;
     line-height: 20px;
@@ -18,13 +44,32 @@ const Tile = styled.div`
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 16px;
     font-weight: 500;
+
+    &:hover ${TileOptions} {
+        visibility: visible;
+    }
+`;
+
+const TileText = styled.div`
+    margin-left: 10px;
+    margin-top: 10px;
 `;
 
 export default function BoardTile(props) {
+
+    const dispatch = useDispatch();
+
+    const handleDeleteBoard = (e) => {
+        dispatch(deleteBoard(props.boardId));
+    }
+
     return (
         <FadeIn>
-            <Tile onClick={props.onClick}>
-                {props.title}
+            <Tile className={"outer"} onClick={props.onClick}>
+                <TileOptions className={"inner"} onMouseDown={handleDeleteBoard}>
+                    <FontAwesomeIcon className={"inner"} icon={faTrash} />
+                </TileOptions>
+                <TileText >{props.title}</TileText>
             </Tile>
         </FadeIn>
     )
